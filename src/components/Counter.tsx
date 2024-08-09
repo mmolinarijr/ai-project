5b579cdd2d30bc81cb0c9fa8ed6c13ed97b1af68
Mauricio Molinari <mmolinarijr@gmail.com>
2024-08-09 15:06:25 +0000

    feat: added counter page

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,37 @@
 'use client';
 
 import React, { useState } from 'react';
+import ReturnButton from '@/components/layout/ReturnButton';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
 
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const decrementButtonStyle = count === 0 ? "bg-red-500 opacity-50 cursor-not-allowed hover:bg-red-500" : "bg-red-500 hover:bg-red-700";
+
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Incrementar</button>
-      <button onClick={() => setCount(count - 1)}>Decrementar</button>
+    <div className="flex flex-col items-center">
+      <ReturnButton />
+      
+      <p className="text-2xl font-bold mb-4">Count: {count}</p>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => setCount(count + 1)}
+      >
+        Incrementar
+      </button>
+      <button
+        className={`mt-4 ${decrementButtonStyle} text-white font-bold py-2 px-4 rounded`}
+        onClick={decrementCount}
+        disabled={count === 0}
+      >
+        Decrementar
+      </button>
     </div>
   );
 };
